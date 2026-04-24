26f722bf950b2b4d5bae7619b61ee9dfcd8758cd
we-all-float <dev.tcg@proton.me>
2026-04-25 09:34:06 +1000

    Add js exercise three

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,9 +1,9 @@
 function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
-    return false;
-  }
+    if (number >= 10) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // Do not edit below this line
